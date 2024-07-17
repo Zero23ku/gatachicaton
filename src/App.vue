d2172b03cb2605362eb758d5c7a30581739d2dd2
Harpuia <zetsubou.harpuia@gmail.com>
2024-07-17 19:44:54 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="menu">
+
+            </div>
+            <h1>Gatachicaton</h1>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
