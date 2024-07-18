6ebe6d83a7c336745c45d3fa89b3e7f3104c5c9e
Harpuia <zetsubou.harpuia@gmail.com>
2024-07-17 22:29:48 -0400

    wip: maqueteo

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,37 @@
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <div class="menu">
-
+    <div class="bg-container">
+        <header>
+            <div class="header-container">
+                <div class="menu grid grid-cols-3 gap-4 content-center text-center">
+                    <div><a href="#">Sobre el evento</a></div>
+                    <div><a href="#">Invitados</a></div>
+                    <div><a href="#">Apoya la causa</a></div>
+                </div>
+                <div class="logo-container">
+                    <div class="flex justify-center items-center">
+                        <img class="logo" src="./assets/logomock.png"></img>
+                    </div>
+                    <div class="text-container">
+                        <h1 class="content-center text-center">Gatachicaton</h1>
+                        <h2 class="content-center text-center -rotate-3"><a href="https://twitch.tv/retniwuwu">twitch.tv/retniuwu</a></h2>
+                    </div>
+                </div>
+                
             </div>
-            <h1>Gatachicaton</h1>
+        </header>
+        <div class="body-container">
+            <div class="img-container flex justify-center">
+                <img src="./assets/gatachica.webp"></img>
+            </div>
+            <div class="text-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec efficitur nunc. In bibendum nisl mi, a maximus leo commodo at. Ut velit massa, dictum ut blandit a, sagittis in purus. Vivamus eu urna vitae nisl aliquam gravida. Aenean porta, quam et porta iaculis, eros orci lacinia nunc, sit amet laoreet nibh lorem in diam. Quisque sollicitudin lacus et dignissim semper. Aenean pharetra arcu in libero pharetra aliquet.
+
+    Phasellus vel iaculis tellus. Nulla non eros consectetur, porta libero eu, sollicitudin libero. Nullam eu pretium magna. Sed sit amet faucibus sapien, ac faucibus quam. Fusce porttitor venenatis consequat. Etiam cursus, dolor ut pellentesque feugiat, ante lacus rhoncus nisl, at vestibulum nunc magna eu ex. Duis nec hendrerit tellus. Nulla semper leo ac lectus vehicula, non maximus metus rhoncus.</p>
+            </div>
         </div>
-    </header>
+    </div>
 </template>
 
 <style scoped>
